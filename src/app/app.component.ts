889ec693e62de7dbf0d44d95b88935d90570aa74
javierfuentesm <javier.fuentesm@hotmail.com>
2019-12-17 23:02:09 -0600

    Input y outputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce';
+  names = [];
+  name: string;
+  products: Product[] = [
+    {
+      id: '1',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addName(name: string) {
+    this.names.push(name);
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
+  }
+  clickedProduct(id: number) {
+    console.log(`producto ${id}`);
+  }
 }
---
+++ src/app/components/product.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../product.model';
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html'
+})
+export class ProductComponent {
+  @Input() product: Product;
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  addCart() {
+    this.productClicked.emit(this.product.id);
+  }
+}
