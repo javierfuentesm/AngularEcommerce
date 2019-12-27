890b1fcc7998c3d581f4327aee7174adbdee3c50
rjfm <javier.fuentesm@hotmail.com>
2019-12-27 13:55:00 -0600

    Se agrega Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
+
 import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { CoreModule } from './core/core.module';
     FormsModule,
     AppRoutingModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProductComponent } from './components/product/product.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ProductRoutingModule } from './product.routing.module';
-import { SharedModule } from '../shared/shared.module';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProductComponent } from "./components/product/product.component";
+import { ProductDetailComponent } from "./components/product-detail/product-detail.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { ProductRoutingModule } from "./product.routing.module";
+import { SharedModule } from "../shared/shared.module";
+import { MaterialModule } from "../material/material.module";
 
 @NgModule({
-  declarations: [ProductComponent, ProductDetailComponent,ProductsComponent],
-  imports: [ProductRoutingModule, CommonModule, SharedModule]
+  declarations: [ProductComponent, ProductDetailComponent, ProductsComponent],
+  imports: [ProductRoutingModule, CommonModule, SharedModule, MaterialModule]
 })
 export class ProductModule {}
