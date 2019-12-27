8bd36458e4a2ad94592bc55f9fbd8ee01e9e5955
rjfm <javier.fuentesm@hotmail.com>
2019-12-27 17:14:34 -0600

    Creacion del header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
-import { HighlightDirective } from './directives/highlight/highlight.directive';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { ExponentialPipe } from "./pipes/exponential/exponential.pipe";
+import { HighlightDirective } from "./directives/highlight/highlight.directive";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { MaterialModule } from "../material/material.module";
 
 @NgModule({
   declarations: [
@@ -19,6 +20,6 @@ import { HeaderComponent } from './components/header/header.component';
     FooterComponent,
     HeaderComponent
   ],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, MaterialModule]
 })
 export class SharedModule {}
