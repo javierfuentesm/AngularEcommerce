eeeafcf1fd23271fb90a674166911483ff64a318
rjfm <javier.fuentesm@hotmail.com>
2020-01-13 11:42:59 -0600

    Se agrega ReacttiveFormModules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { FooterComponent } from './components/footer/footer.component';
@@ -20,6 +21,6 @@ import { MaterialModule } from '../material/material.module';
     FooterComponent,
     HeaderComponent
   ],
-  imports: [CommonModule, RouterModule, MaterialModule]
+  imports: [CommonModule, RouterModule, MaterialModule, ReactiveFormsModule]
 })
 export class SharedModule {}
